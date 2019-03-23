612aaea79d206ad8326ea522df96d527a58f5910
unknown <dbrudne1@gmail.com>
2019-03-23 12:20:09 -0500

    committing before starting adding testing/jest

--- schema/cards/field.js
+++ schema/cards/field.js
@@ -8,29 +8,29 @@ module.exports = {
 	args: {
 		name: {
 			type: GraphQLString,
-			description: "Do a search for a card by name."
+			description: "Do a search for a card by name.",
 		},
 		text: {
 			type: GraphQLString,
-			description: "Do a search for a card by description."
-		}
+			description: "Do a search for a card by description.",
+		},
 	},
 	async resolve(parentValue, args) {
 		const response = await fetch(
-			"https://api.hearthstonejson.com/v1/25770/enUS/cards.collectible.json"
+			"https://api.hearthstonejson.com/v1/25770/enUS/cards.collectible.json",
 		);
 
 		const data = await response.json();
 		const searchCardProperty = key => card =>
 			card[key] && card[key].includes(args[key] || "");
 
-		const filterByName = _.filter(searchCardProperty("name"));
-		const filterByText = _.filter(searchCardProperty("text"));
+		const filterByKey = key => _.filter(searchCardProperty(key));
+
 		const search = _.compose(
-			filterByText,
-			filterByName
+			filterByKey("name"),
+			filterByKey("text"),
 		);
 
 		return search(data);
-	}
+	},
 };
