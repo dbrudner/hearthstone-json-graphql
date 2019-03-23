27e70b9952392eaf3ada70fdc12e9320232611de
Dave Brudner <dbrudne1@gmail.com>
2019-03-23 00:29:32 -0500

    args working

--- schema/cards/field.js
+++ schema/cards/field.js
@@ -8,45 +8,29 @@ module.exports = {
 	args: {
 		name: {
 			type: GraphQLString,
-			description: "Do a search for a card by name.",
+			description: "Do a search for a card by name."
 		},
-		description: {
+		text: {
 			type: GraphQLString,
-			description: "Do a search for a card by description.",
-		},
+			description: "Do a search for a card by description."
+		}
 	},
 	async resolve(parentValue, args) {
 		const response = await fetch(
-			"https://api.hearthstonejson.com/v1/25770/enUS/cards.collectible.json",
+			"https://api.hearthstonejson.com/v1/25770/enUS/cards.collectible.json"
 		);
 
 		const data = await response.json();
+		const searchCardProperty = key => card =>
+			card[key] && card[key].includes(args[key] || "");
 
-		const searchOne = key => _.compose(_.map(key));
-
-		const filterByName = searchOne("name");
-		const filterByText = searchOne("text");
-
+		const filterByName = _.filter(searchCardProperty("name"));
+		const filterByText = _.filter(searchCardProperty("text"));
 		const search = _.compose(
-			filterByName,
 			filterByText,
+			filterByName
 		);
 
-		// const filterByText = data.filter(({ text }) => {
-		// 	if (text) {
-		// 		return text.includes(args.text);
-		// 	}
-		// });
-
-		// const filterCards = _.compose(
-		// 	filterByName,
-		// 	filterByText,
-		// );
-
-		// const x = filterCards(data);
-		// console.log(x);
-
-		// return filterCards(data);
 		return search(data);
-	},
+	}
 };
