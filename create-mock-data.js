a9302ed8ebff23ddabe8b4648b46d1ff829df0f4
unknown <dbrudne1@gmail.com>
2019-03-24 20:14:45 -0500

    publishing test results

--- create-mock-data.js
+++ create-mock-data.js
@@ -5,6 +5,7 @@ const fetchAndWriteToJSON = async () => {
 	const response = await fetch(
 		"https://api.hearthstonejson.com/v1/25770/enUS/cards.collectible.json",
 	);
+
 	const data = await response.json();
 
 	fs.writeFileSync("./test/mock-data.json", JSON.stringify(data));
