45ff399054b54f11ce3d777d1b459ef7ad07db48
unknown <dbrudne1@gmail.com>
2019-03-31 13:28:39 -0500

    large test

--- schema/cards/query/test.js
+++ schema/cards/query/test.js
@@ -13,4 +13,61 @@ describe("full query with real data", () => {
 
 		expect(expected.length).toBe(1);
 	});
+
+	it("should pass a test with real data", () => {
+		const actual = [
+			{
+				name: "Aluneth",
+			},
+			{
+				name: "Anomalus",
+			},
+			{
+				name: "Archmage Antonidas",
+			},
+			{
+				name: "Archmage Arugal",
+			},
+			{
+				name: "Dragoncaller Alanna",
+			},
+			{
+				name: "Flame Leviathan",
+			},
+			{
+				name: "Frost Lich Jaina",
+			},
+			{
+				name: "Inkmaster Solia",
+			},
+			{
+				name: "Luna's Pocket Galaxy",
+			},
+			{
+				name: "Open the Waygate",
+			},
+			{
+				name: "Pyros",
+			},
+			{
+				name: "Rhonin",
+			},
+			{
+				name: "Sindragosa",
+			},
+			{
+				name: "Stargazer Luna",
+			},
+			{
+				name: "Toki, Time-Tinker",
+			},
+		];
+
+		const expected = query(cards, {
+			filter: { cardClass: "MAGE", rarity: "LEGENDARY" },
+			sort: { by: "name" },
+		});
+
+		expect(expected.map(({ name }) => ({ name }))).toEqual(actual);
+	});
 });
