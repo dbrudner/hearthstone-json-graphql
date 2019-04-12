05f5dbb85a96db758a9897cf644aeb77587928d8
unknown <dbrudne1@gmail.com>
2019-04-11 21:22:19 -0500

    add types meta

--- schema/meta/field.js
+++ schema/meta/field.js
@@ -1,9 +1,9 @@
 const Meta = require("./schema");
-const { sets, cardClasses, rarities } = require("../../data/cards");
+const { sets, cardClasses, rarities, types } = require("../../data/cards");
 
 module.exports = {
 	type: Meta,
 	async resolve() {
-		return { cardClasses, rarities, sets };
+		return { cardClasses, rarities, sets, types };
 	},
 };
