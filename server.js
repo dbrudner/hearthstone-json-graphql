a5b083087dbd9f839ebc7772d88c4e145d463d1d
Dave Brudner <dbrudne1@gmail.com>
2019-04-29 17:17:26 -0500

    versioning and index page

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
@@ -7,13 +8,17 @@ const app = express();
 app.use(cors());
 
 app.use(
-	"/",
+	"/v1",
 	graphqlHTTP({
 		schema: schema,
-		graphiql: true,
-	}),
+		graphiql: true
+	})
 );
 
 app.use("/docs", express.static("docs"));
+
+app.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/pages", "/index.html"));
+});
 
 app.listen(process.env.PORT || 3000);
