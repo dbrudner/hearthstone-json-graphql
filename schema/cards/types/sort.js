a9b5c9f36b9143ccfa758d9c925ae53f3670c580
unknown <dbrudne1@gmail.com>
2019-05-11 14:33:33 -0500

    sort enum args

--- schema/cards/types/sort.js
+++ schema/cards/types/sort.js
@@ -1,13 +1,40 @@
-const { GraphQLString, GraphQLInputObjectType } = require("graphql");
+const {
+	GraphQLString,
+	GraphQLInputObjectType,
+	GraphQLEnumType,
+} = require("graphql");
 
 module.exports = new GraphQLInputObjectType({
 	name: "Sorted",
 	fields: {
 		by: {
-			type: GraphQLString,
+			type: new GraphQLEnumType({
+				name: "by",
+				values: {
+					COST: {
+						value: "cost",
+					},
+					HEALTH: {
+						value: "health",
+					},
+					ATTACK: {
+						value: "attack",
+					},
+				},
+			}),
 		},
 		direction: {
-			type: GraphQLString,
+			type: new GraphQLEnumType({
+				name: "direction",
+				values: {
+					ASCENDING: {
+						value: "asc",
+					},
+					DESCENDING: {
+						value: "desc",
+					},
+				},
+			}),
 		},
 	},
 });
