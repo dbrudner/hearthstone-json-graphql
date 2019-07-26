73127aa24702590727082d6dc6852147fb429602
unknown <dbrudne1@gmail.com>
2019-07-26 15:24:45 -0500

    merge

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== "production") {
-	require("dotenv").config();
-}
 const path = require("path");
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
